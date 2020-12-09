b2e271726afb770915d36bf845ec7df24e21affa
Yoichi-dev <yoichi.developer@gmail.com>
2020-12-09 21:59:48 +0900

    初期設定

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      htmlAttrs: {
+        lang: 'ja'
+      },
+    }
+  }
+}
+</script>
+
+<style>
+</style>
